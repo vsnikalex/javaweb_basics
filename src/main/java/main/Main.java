40fc4590b2e7f287bda60559e255374ac51cfd75
Vasily <vsnikalex@gmail.com>
2019-06-02 15:47:53 +0300

    Add implementation: Main.main() - server start

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,23 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import servlets.AllRequestsServlet;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
     }
 }
