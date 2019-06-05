c2a8f1017d1853be571c27c167b0587072d77044
Vasily <vsnikalex@gmail.com>
2019-06-05 14:43:43 +0300

    HW02 structure preparation

---
+++ src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import accounts.AccountService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SignInServlet extends HttpServlet {
+    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"}) //todo: remove after module 2 home work
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //get public user profile
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+
+    }
+
+}
